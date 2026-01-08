a7d60b831399e68007419fb5ac23b5f1d23af135
RasulIbr <mrmrasul.7@gmail.com>
2026-01-08 13:51:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+// Тестируемая переменная (можно менять значение для проверки)
+$variable = 3.14;
+// $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+// $variable = [];
+
+$type = '';
+
+if (is_bool($variable)) {
+    $type = 'bool';
+} elseif (is_float($variable)) {
+    $type = 'float';
+} elseif (is_int($variable)) {
+    $type = 'int';
+} elseif (is_string($variable)) {
+    $type = 'string';
+} elseif (is_null($variable)) {
+    $type = 'null';
+} else {
+    $type = 'other';
+}
+
+echo "type is $type";
